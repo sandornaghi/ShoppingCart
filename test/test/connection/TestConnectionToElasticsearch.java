c65f0010cdaddb8cccd0345ada46a33aff231871
Sandor Naghi <sandor.naghi@fortech.ro>
2017-01-31 16:16:28 +0200

    Starting Unit testing

---
+++ test/test/connection/TestConnectionToElasticsearch.java
@@ -0,0 +1,12 @@
+package test.connection;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestConnectionToElasticsearch {
+
+	@Test
+	public void checkForConnection() {
+		assertTrue(true);
+	}
+}
